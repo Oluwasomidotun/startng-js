7223fc5837bf57737e8e18a600c78fc2c9b7ca33
Oluwasomidotun <hexcel9@gmail.com>
2020-03-28 15:59:12 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+var name = "Oluwasomidotun Oladipupo";
+var courses = ["HTML",  "CSS", "JS", "Python", "Ruby", "GO", "C#", "NodeJS", "PHP"];
+var number = courses.length;
+for (i=1; i <= 200; i+=2) {
+  if (number % 2 !== 0 ) 
+    
+      console.log(i); 
+      }
